55cd897e44c59c87c03b1a03d29df24e67fefae2
Pramika Garg <micepram@gmail.com>
2025-01-05 10:13:48 +0530

    feat: add lexer class skeleton

--- posix_lexer.py
+++ posix_lexer.py
@@ -1,1 +1,9 @@
 """POSIX Token Recognition Lexer (IEEE 1003.1-2017 Section 2.3)"""
+
+class POSIXLexer:
+    def __init__(self, input_str: str):
+        self.input = input_str
+        self.tokens = []
+
+    def tokenize(self) -> list[dict]:
+        return []

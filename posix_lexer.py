702513c4a49517381a16a2c4342db0251cd87855
Pramika Garg <micepram@gmail.com>
2025-01-19 14:06:22 +0530

    refactor: implement position tracking helpers

--- posix_lexer.py
+++ posix_lexer.py
@@ -3,7 +3,17 @@
 class POSIXLexer:
     def __init__(self, input_str: str):
         self.input = input_str
+        self.length = len(input_str)
+        self.pos = 0
         self.tokens = []
+
+    def _advance(self, n=1):
+        self.pos += n
+        if self.pos > self.length:
+            self.pos = self.length
+
+    def _current_char(self):
+        return self.input[self.pos] if self.pos < self.length else None
 
     def tokenize(self) -> list[dict]:
         return []

3844f9327eff95c23b75e8767112b79b1a69d326
Pramika Garg <micepram@gmail.com>
2025-05-03 09:24:17 +0530

    test: add quoting edge case tests

--- test_posix_lexer.py
+++ test_posix_lexer.py
@@ -4,7 +4,38 @@ from posix_lexer import POSIXLexer
 class TestPOSIXLexer(unittest.TestCase):
     def test_empty_input(self):
         lexer = POSIXLexer('')
-        self.assertEqual(lexer.tokenize(), [])
+        self.assertEqual(lexer.get_all_tokens(), [])
+
+    def test_quoted_blanks(self):
+        lexer = POSIXLexer("'hello world' \"foo bar\"")
+        expected = [
+            {'type': 'word', 'value': 'hello world'},
+            {'type': 'word', 'value': 'foo bar'}
+        ]
+        self.assertEqual(lexer.get_all_tokens(), expected)
+
+    def test_mixed_quotes(self):
+        lexer = POSIXLexer('"foo"\'bar\' unquoted')
+        expected = [
+            {'type': 'word', 'value': 'foobar'},
+            {'type': 'word', 'value': 'unquoted'}
+        ]
+        self.assertEqual(lexer.get_all_tokens(), expected)
+
+    def test_escapes_in_double(self):
+        lexer = POSIXLexer(r'"\""')
+        expected = [{'type': 'word', 'value': '"'}]
+        self.assertEqual(lexer.get_all_tokens(), expected)
+
+    def test_single_quotes_literal(self):
+        lexer = POSIXLexer(r"'\t'")
+        expected = [{'type': 'word', 'value': '\\t'}]
+        self.assertEqual(lexer.get_all_tokens(), expected)
+
+    def test_escaped_unquoted(self):
+        lexer = POSIXLexer(r'foo\ bar')
+        expected = [{'type': 'word', 'value': 'foo bar'}]
+        self.assertEqual(lexer.get_all_tokens(), expected)
 
 if __name__ == '__main__':
     unittest.main()
